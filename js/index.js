776462508dd17309c9fdeaae3c4469684d45da0e
Sam van Berkel <sam@posonas.com>
2020-01-21 22:10:20 +0100

    Sign in system

--- js/index.js
+++ js/index.js
@@ -1,31 +1,108 @@
-var pathfinding = {
-    set: {
-        grid: function() {
-            const cellWidth = 30;
+if (!window.bruin) var bruin = {
+    rest: {
+        get: {
+            songs: function() {
+                let result = $.ajax({
+                    url: "https://api.myjson.com/bins/iruli",
+                    type:"GET",
+                    contentType:"application/json; charset=utf-8",
+                    dataType:"json",
+                    success: function(data, textStatus, jqXHR){
+                
+                    }
+                });
 
-            const gridDiv = document.getElementById('grid');
-        
-            
-            const gridWidth = gridDiv.clientWidth;
-            const gridHeight = gridDiv.clientHeight;
+                return result;
+            },
+            users: function() {
+                let result = $.ajax({
+                    url: "https://api.myjson.com/bins/zw28m",
+                    type:"GET",
+                    contentType:"application/json; charset=utf-8",
+                    dataType:"json",
+                    success: function(data, textStatus, jqXHR){
+                
+                    }
+                });
 
-            const amountOfColumns = gridWidth / cellWidth;
-            const amountOfRows = gridHeight / cellWidth;
+                return result;
+            }
+        },
+        put: {
+            users: function(users) {
+                let result = $.ajax({
+                    url: "https://api.myjson.com/bins/zw28m",
+                    type:"PUT",
+                    data: JSON.stringify(users),
+                    contentType:"application/json; charset=utf-8",
+                    dataType:"json",
+                    success: function(data, textStatus, jqXHR){
+                
+                    }
+                });
 
-            gridDiv.style.gridTemplateColumns = `repeat(${cellWidth}px, ${amountOfColumns})`;
-            gridDiv.style.gridTemplateRows = `repeat(${cellWidth}px, ${amountOfRows})`;
-
-            for (let c = 0; c < amountOfColumns; c++) {
-                for (let r = 0; r < amountOfRows; r++) {
-                    const cellDiv = document.createElement('div');
-                    cellDiv.classList.add('gridCell');
-                }
+                return result;
             }
         }
     },
-    init: function() {
-        pathfinding.set.grid();
+    data: {
+        songs: [],
+        users: []
+    },
+    clear: function() {
+        Id('tbFirstName').value = "",
+        Id('tbLastName').value = "",
+        Id('tbUserName').value = "",
+        Id('tbPassword').value = ""
+    },
+    init: async function() {
+        if (localStorage.getItem('userName')) {
+            window.location.href = "signedIn.html";
+        }
+
+        Id('btnJoin').onclick = async function() {
+            let newUserObj = {
+                firstName: Id('tbFirstName').value,
+                lastName: Id('tbLastName').value,
+                userName: Id('tbUserName').value,
+                password: Id('tbPassword').value
+            }
+
+            const users = await bruin.rest.get.users();
+
+            let duplicateUsername = false;
+
+            users.forEach(function(user) {
+                if (user.userName == newUserObj.userName) {
+                    duplicateUsername = true;
+                }
+            })
+
+            if (duplicateUsername) {
+                return;
+            }
+
+            users.push(newUserObj);
+            await bruin.rest.put.users(users);
+
+            bruin.clear();
+
+            localStorage.setItem('userName', newUserObj.userName);
+            window.location.href = "signedIn.html";
+        }
+
+        Id('btnSignIn').onclick = async function() {
+
+        }
+
+        bruin.data.songs = await bruin.rest.get.songs();
     }
 }
 
-window.onload = pathfinding.init();
+var Id = function(id) {
+    return document.getElementById(id);
+}
+
+$( document ).ready(function() {
+    bruin.init();
+});
