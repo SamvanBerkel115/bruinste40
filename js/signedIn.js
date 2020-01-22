4151bd863f5cd097f120348209bb71eb13b8db3b
Sam van Berkel <sam@posonas.com>
2020-01-22 19:31:08 +0100

    Song selection styling

--- js/signedIn.js
+++ js/signedIn.js
@@ -42,46 +42,133 @@ if (!window.bruin) var bruin = {
                 });
 
                 return result;
+            },
+            songs: function(songs) {
+                let result = $.ajax({
+                    url: "https://api.myjson.com/bins/iruli",
+                    type:"PUT",
+                    data: JSON.stringify(songs),
+                    contentType:"application/json; charset=utf-8",
+                    dataType:"json",
+                    success: function(data, textStatus, jqXHR){
+                
+                    }
+                });
+
+                return result;
             }
         }
     },
     data: {
         songs: [],
+        searchedSongs: [],
+        selectedSongs: [],
         users: [],
         chosen: []
     },
     set: {
         songs: function() {
-            
+            Id('divSongs').innerHTML = "";
+
+            bruin.data.searchedSongs.forEach(function(song) {
+                let divSong = document.createElement('div');
+                divSong.classList.add('divSong');
+                divSong.params = song;
+    
+                let pTrack = document.createElement('p');
+                pTrack.innerHTML = song.track;
+                pTrack.classList.add('pTrack');
+                divSong.appendChild(pTrack);
+    
+                let pArtist = document.createElement('p');
+                pArtist.innerHTML = song.artist;
+                pArtist.classList.add('pArtist');
+                divSong.appendChild(pArtist);
+    
+                divSong.onclick = function(evt) {
+                    let divSong = this;
+
+                    if (bruin.data.selectedSongs.length >= 10) {
+                        return;
+                    }
+
+                    for (let i = 0; i < bruin.data.selectedSongs.length; i++) {
+                        if (bruin.data.selectedSongs[i].track == divSong.params.track) {
+                            return;
+                        }
+                    }
+
+                    divSong.style.opacity = "0.5"
+
+                    bruin.data.selectedSongs.push(divSong.params);
+
+                    let divSelectedSong = document.createElement('li');
+                    divSelectedSong.classList.add('divSelectedSong');
+                    divSelectedSong.classList.add('ui-state-default"');
+                    divSelectedSong.params = divSong.params;
+
+                    let pTrack = document.createElement('span');
+                    pTrack.innerHTML = divSong.params.track;
+                    pTrack.classList.add('pTrack');
+                    divSelectedSong.appendChild(pTrack);
+    
+                    Id('divSelectedSongs').appendChild(divSelectedSong);
+                }
+    
+                Id('divSongs').appendChild(divSong);
+            })
         }
     },
     clear: function() {
     },
     init: async function() {
-        bruin.data.songs = await bruin.rest.get.songs();
+        Id('tbSearchBar').oninput = function(evt) {
+            let text = this.value;
 
-        bruin.data.songs.forEach(function(song) {
-            let divSong = document.createElement('div');
-            divSong.classList.add('divSong');
+            let terms = text.split(" ").filter(function(term) {
+                
+                if (term == "") {
+                    return false;
+                } else if (term.length < 2) {
+                    return false;
+                } else {
+                    return true;
+                }
+            });
 
-            let pTrack = document.createElement('p');
-            pTrack.innerHTML = song.track;
-            pTrack.classList.add('pTrack');
-            divSong.appendChild(pTrack);
-
-            let pArtist = document.createElement('p');
-            pArtist.innerHTML = song.artist;
-            pArtist.classList.add('pArtist');
-            divSong.appendChild(pArtist);
-
-            divSong.onclick = function(evt) {
-                let songDiv = this.cloneNode(true);
-
-                Id('divSelection').appendChild(songDiv);
+            if (terms.length == 0) {
+                bruin.data.searchedSongs = bruin.data.songs;
+            } else {
+                bruin.data.searchedSongs = bruin.data.songs.filter(function(song) {
+                    let matched = false
+                    
+                    terms.forEach(function(term) {
+                        if (song.track.toLowerCase().includes(term.toLowerCase())) {
+                            matched = true;
+                        }
+    
+                        if (song.artist.toLowerCase().includes(term.toLowerCase())) {
+                            matched = true;
+                        }
+                    })
+    
+                    if (matched) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                    
+                })
             }
 
-            Id('divSongs').appendChild(divSong);
-        })
+            bruin.set.songs();
+        }
+
+        $("#divSelectedSongs").sortable();
+        $("#divSelectedSongs").disableSelection();
+
+        bruin.data.songs = bruin.data.searchedSongs = await bruin.rest.get.songs();
+        bruin.set.songs();
     }
 }
 
