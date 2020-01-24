6a537f5706a7a329dc10dc56038d8aa95a95c37b
Sam van Berkel <sam@posonas.com>
2020-01-24 13:56:22 +0100

    Reset

--- js/signedIn.js
+++ js/signedIn.js
@@ -332,29 +332,7 @@ if (!window.bruin) var bruin = {
 
         let requiredHeight = parseInt(Id('numberList').offsetHeight, 10) + 50;
         Id('divJouwBruine').style.height = requiredHeight + 'px';
-
-        document.addEventListener("touchstart", touchHandler, true);
-        document.addEventListener("touchmove", touchHandler, true);
-        document.addEventListener("touchend", touchHandler, true);
-        document.addEventListener("touchcancel", touchHandler, true);
     }
-}
-
-function touchHandler(event) {
-    var touch = event.changedTouches[0];
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-    simulatedEvent.initMouseEvent({
-            touchstart: "mousedown",
-            touchmove: "mousemove",
-            touchend: "mouseup"
-        }[event.type], true, true, window, 1,
-        touch.screenX, touch.screenY,
-        touch.clientX, touch.clientY, false,
-        false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
 }
 
 var Id = function(id) {
