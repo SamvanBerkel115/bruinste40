6823942a497c8f9b6d4fab4f0bfba1a219b71e8a
Sam van Berkel <sam@posonas.com>
2020-01-24 13:51:24 +0100

    Sortable bugfix

--- js/signedIn.js
+++ js/signedIn.js
@@ -301,6 +301,8 @@ if (!window.bruin) var bruin = {
             }
         });
         $("#divSelectedSongs").disableSelection();
+        $("#divSelectedSongs").sortable("option", "axis", "y");
+        $("#myContainer").sortable("option", "containment", Id('divJouwBruine'));
 
         bruin.data.songs = bruin.data.searchedSongs = await bruin.rest.get.songs();
         bruin.data.users = await bruin.rest.get.users();
@@ -330,7 +332,29 @@ if (!window.bruin) var bruin = {
 
         let requiredHeight = parseInt(Id('numberList').offsetHeight, 10) + 50;
         Id('divJouwBruine').style.height = requiredHeight + 'px';
+
+        document.addEventListener("touchstart", touchHandler, true);
+        document.addEventListener("touchmove", touchHandler, true);
+        document.addEventListener("touchend", touchHandler, true);
+        document.addEventListener("touchcancel", touchHandler, true);
     }
+}
+
+function touchHandler(event) {
+    var touch = event.changedTouches[0];
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent({
+            touchstart: "mousedown",
+            touchmove: "mousemove",
+            touchend: "mouseup"
+        }[event.type], true, true, window, 1,
+        touch.screenX, touch.screenY,
+        touch.clientX, touch.clientY, false,
+        false, false, false, 0, null);
+
+    touch.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
 }
 
 var Id = function(id) {
