b53c2b025748f809b09195ce69bbdf259dbc176b
Sam van Berkel <sam@posonas.com>
2020-01-24 13:48:32 +0100

    Sortable bugfix

--- js/signedIn.js
+++ js/signedIn.js
@@ -224,28 +224,6 @@ if (!window.bruin) var bruin = {
     },
     clear: function() {
     },
-    sortableFix: function() {
-        function touchHandler(event) {
-            var touch = event.changedTouches[0];
-        
-            var simulatedEvent = document.createEvent("MouseEvent");
-            simulatedEvent.initMouseEvent({
-                    touchstart: "mousedown",
-                    touchmove: "mousemove",
-                    touchend: "mouseup"
-                }[event.type], true, true, window, 1,
-                touch.screenX, touch.screenY,
-                touch.clientX, touch.clientY, false,
-                false, false, false, 0, null);
-        
-            touch.target.dispatchEvent(simulatedEvent);
-        }
-        
-        document.addEventListener("touchstart", touchHandler, true);
-        document.addEventListener("touchmove", touchHandler, true);
-        document.addEventListener("touchend", touchHandler, true);
-        document.addEventListener("touchcancel", touchHandler, true);
-    },
     init: async function() {
         Id('tbSearchBar').oninput = function(evt) {
             let text = this.value;
